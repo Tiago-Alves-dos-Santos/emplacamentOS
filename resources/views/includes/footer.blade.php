7ea254f8be96ca0e9d81173a91686a1c4de70fed
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-06 21:30:46 -0300

    estrututra inicial montada

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,4 @@
+
+@livewireScripts()
+<script src="{{mix('js/app.js')}}"></script>
+<script src="{{asset('js/footer.js')}}"></script>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('includes.header')
+<body>
+    <div class="container-fluid">
+        @yield('body')
+    </div>
+    @include('includes.footer')
+</body>
+</html>
