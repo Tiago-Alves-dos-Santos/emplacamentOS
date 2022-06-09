f82e2d5bf556cc8f3ab09d86e1c772ea055e1452
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-08 23:14:32 -0300

    migration notificação com model configurada

---
+++ database/migrations/2022_06_08_230909_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo',255)->nullable();
+            $table->text('descricao')->nullable();
+            $table->string('tabela',255)->nullable();
+            $table->bigInteger('id')->unsigned()->nullable();//se id for nulo, mandar notificação pra geral
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
