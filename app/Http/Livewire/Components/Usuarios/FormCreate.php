69357563f67ecbde6f340547d0232143d249d3d4
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-09 21:37:31 -0300

    paginas de usuario components criados

---
+++ app/Http/Livewire/Components/Usuarios/FormCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\Usuarios;
+
+use Livewire\Component;
+
+class FormCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.usuarios.form-create');
+    }
+}
---
+++ app/Http/Livewire/Components/Usuarios/FormUpdate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\Usuarios;
+
+use Livewire\Component;
+
+class FormUpdate extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.usuarios.form-update');
+    }
+}
