fc83b42b346a90c047c192a150eaa46cd8694820
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-13 11:11:22 -0300

    tabela usuarios configurada

--- resources/views/livewire/components/cliente/table.blade.php
+++ resources/views/livewire/components/cliente/table.blade.php
@@ -1,3 +1,35 @@
 <div>
     {{-- The best athlete wants his opponent at his best. --}}
+    <div class="row mb-3">
+        <div class="col-md-11">
+            <input type="search" class="form-control" placeholder="PESQUISAR">
+        </div>
+        <div class="col-md">
+            <a href="{{route('view.user.create')}}" class="btn btn-info d-block">
+                ADICIONAR
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                    <thead>
+                        <th>Nome</th>
+                        <th>Telefone</th>
+                        <th>Nascimento</th>
+                        <th>Idade</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>teste</td>
+                            <td>teste</td>
+                            <td>teste</td>
+                            <td>teste</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/livewire/pages/cliente/dashboard.blade.php
+++ resources/views/livewire/pages/cliente/dashboard.blade.php
@@ -1,4 +1,4 @@
 <div>
     {{-- Success is as dangerous as failure. --}}
-    Clietnes aq
+    <livewire:components.cliente.table>
 </div>
