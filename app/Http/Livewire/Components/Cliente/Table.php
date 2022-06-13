19cc0986e8d2266ba90deafb058199c711d892b4
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-13 10:49:54 -0300

    compoentns e paginas de clietne, componentes criado

---
+++ app/Http/Livewire/Components/Cliente/Table.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\Cliente;
+
+use Livewire\Component;
+
+class Table extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.cliente.table');
+    }
+}
