422d812f1c6607a1d60a5aa2a828b2eceb2c6c83
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-13 16:06:48 -0300

    compoents e pagina de servicos criadas

---
+++ app/Http/Livewire/Components/Servico/FormCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\Servico;
+
+use Livewire\Component;
+
+class FormCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.servico.form-create');
+    }
+}
