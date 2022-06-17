625dddf6d85745c3db151d8554c285f49f5ed1d0
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-17 11:00:01 -0300

    input search estilazdo com img

--- resources/views/livewire/components/os/search-cliente.blade.php
+++ resources/views/livewire/components/os/search-cliente.blade.php
@@ -1,7 +1,7 @@
 <div>
     {{-- Close your eyes. Count to one. That is how long forever feels. --}}
-    <input type="search" wire:model='search' class="select-search form-control">
-    <select name="" id="" class="custom-select" multiple>
+    <input type="search" wire:model='search' class="select-search form-control" placeholder="Nome cliente">
+    <select name="" id="" class="custom-select">
         <option value="" selected>Selecione</option>
         @foreach ($clientes as $value)
             <option value="{{$value->id}}">{{$value->nome}}</option>
