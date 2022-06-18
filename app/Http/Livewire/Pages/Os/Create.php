ff7ee87a2f87d78eb2e3d035722e74880444d1fb
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-18 14:00:03 -0300

    removido search cliente

--- app/Http/Livewire/Pages/Os/Create.php
+++ app/Http/Livewire/Pages/Os/Create.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Pages\Os;
 
+use App\Models\Cliente;
 use App\Models\Servico;
 use Livewire\Component;
 use App\Http\Classes\Configuracao;
@@ -9,6 +10,8 @@ use App\Http\Classes\Configuracao;
 class Create extends Component
 {
     public $cliente_id = 0;
+    public $search_cliente = "";
+    public $search = "";
     protected $listeners = [
         'os.reload.reload' => '$refresh',
     ];
@@ -22,7 +25,12 @@ class Create extends Component
 
     public function render()
     {
-        return view('livewire.pages.os.create',)
+        return view('livewire.pages.os.create',[
+            'servicos' => ($this->search == "")?[]:
+            Servico::where('nome','like', "%{$this->search}%")
+            ->paginate(10),
+            'clientes' => ($this->search_cliente == "")? Cliente::limit(50)->get() : Cliente::where('nome', 'like', "%{$this->search_cliente}%")->get()
+        ])
         ->extends('layouts.admin', ['page_active' => 'os.create'])
         ->section('conteudo');
     }
