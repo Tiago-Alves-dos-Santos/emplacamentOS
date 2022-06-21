dbbb160e66afd7c1ca3516d98c4275f2ea31fa2e
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-21 10:57:18 -0300

    components veiculo de cliente

---
+++ app/Http/Livewire/Components/Veiculo/FormCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\Veiculo;
+
+use Livewire\Component;
+
+class FormCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.veiculo.form-create');
+    }
+}
---
+++ app/Http/Livewire/Components/Veiculo/Table.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components\Veiculo;
+
+use Livewire\Component;
+
+class Table extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.veiculo.table');
+    }
+}
