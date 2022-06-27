d774bbcc8d33f41251f31aadc1d7adef2dcf1ed6
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-27 16:10:52 -0300

    component estilizado para receber as msg

--- resources/views/livewire/components/notificacao/lista.blade.php
+++ resources/views/livewire/components/notificacao/lista.blade.php
@@ -1,4 +1,22 @@
 <div>
     {{-- Do your work, then step back. --}}
-    eu sou a lista components
+    <div class="shadow p-3 mb-5 bg-white rounded">
+        <div class="row">
+            <div class="col-md-4 d-flex justify-content-start">
+                <h2>
+                    1
+                </h2>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <h4>
+                    eu sou a lista components
+                </h4>
+            </div>
+            <div class="col-md-4 d-flex justify-content-end">
+                <button class="btn btn-success">
+                    Marcar como lida
+                </button>
+            </div>
+        </div>
+    </div>
 </div>
