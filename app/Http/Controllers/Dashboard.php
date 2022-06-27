7dcbcf9e93d2a85957a65d4394891d80f141fbb7
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-27 16:03:14 -0300

    ajustes

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -40,30 +40,7 @@ class Dashboard extends Controller
             ];
 
             return json_encode($retorno);
-        }else{
-            $data = explode('-',date('Y-m'));
-        }
-
-        return view('pages.dashboard', [
-            'total_clientes' => Cliente::count(),
-            //despezas esse mes
-            'total_despeza_mensal' => Despezas::whereMonth('mes_referente', $data[1])
-            ->whereYear('mes_referente', $data[0])
-            ->sum('valor'),
-            //os realizadas esse mes
-            'os_mes_atual' => OS::whereMonth('created_at', $data[1])
-            ->whereYear('created_at', $data[0])
-            ->count(),
-            //grafico de total - taxas e lucro das os desse mes
-            'os_lucro_mensal' => OS::lucroMensal($data[1], $data[0])
-        ]);
-    }
-
-    public function dataDashboard(Request $request)
-    {
-        if($request->ajax()){
-            $data =  explode('-',$request->data);
-
         }
     }
+
 }
