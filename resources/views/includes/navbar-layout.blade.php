b34ee9467bda8fc17542ca1ca7f8f088f7848746
Tiago <tiagooliveiraaso2@gmail.com>
2022-06-28 17:57:03 -0300

    ajustador profile responsivo com img

--- resources/views/includes/navbar-layout.blade.php
+++ resources/views/includes/navbar-layout.blade.php
@@ -123,8 +123,8 @@
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auths::user()->name}}</span>
-                {{-- <img class="img-profile rounded-circle"
-                    src="img/undraw_profile.svg"> --}}
+                <img class="img-profile rounded-circle"
+                    src="{{asset('img/operating_system_48px.png')}}">
 
             </a>
             <!-- Dropdown - User Information -->
