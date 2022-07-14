716a807e6982b23bfba64c63eb9eefde50692e14
Tiago <tiagooliveiraaso2@gmail.com>
2022-07-14 10:48:38 -0300

    ajuste link imagem

--- resources/views/includes/sidebar-layout.blade.php
+++ resources/views/includes/sidebar-layout.blade.php
@@ -1,7 +1,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex flex-column align-items-center justify-content-start" href="index.html">
+    <a class="sidebar-brand d-flex flex-column align-items-center justify-content-start" href="{{route('home')}}">
         <div class="sidebar-brand-icon">
             {{-- <i class="fas fa-laugh-wink"></i> --}}
             <img src="{{asset('img/logo_menu.png')}}" class="img-fluid" alt="">
